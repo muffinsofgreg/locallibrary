0b78308088f4f81132dda6747d05641fa9ddc89f
Greg Griffin <griffin.ge@gmail.com>
2018-03-17 10:45:22 -0700

    User authentication and password reset templates complete

--- catalog/views.py
+++ catalog/views.py
@@ -17,6 +17,10 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # all() is implied by default
 
+    # Number of visits to this view, as counted in session variable
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Render the html template index.html with the data in the context variable
     return render(request, 'index.html', context={
         'num_books': num_books,
@@ -24,6 +28,7 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     })
 
 
