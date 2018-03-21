935d93155419d1aef10e2e86cc3ef1926225ef81
Greg Griffin <griffin.ge@gmail.com>
2018-03-20 23:24:25 -0700

    created form view and url

--- catalog/views.py
+++ catalog/views.py
@@ -6,6 +6,13 @@ from .models import Book, Author, BookInstance, Genre
 # For authentication and permissions
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+# For forms
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import RenewBookForm
+import datetime
+
 
 def index(request):
     """
@@ -77,3 +84,34 @@ class AllBorrowedBooksListView(PermissionRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(status="o")
+
+
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    # If this is a POST rquest then process the Form data
+    if request.method == 'POST':
+
+        # Create a Form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+
+            # Process the data in the form.cleaned_data as required
+            # (here we just write it to the model due_back field.)
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    # If this is a GET (or any other method) create the default form
+    else:
+        proposed_renewal_date = datetime.date.today() \
+                              + datetime.timedelta(weeks=3)
+
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date, })
+
+    return render(request, 'catalog/book_renew_librarian.html',
+                  {'form': form, 'bookinst': book_inst})
